3c40afa7debc8cc66ff7fea67adf5f8f0b1c0bed
Chigoziri <ChigoziriOnwenu@gmail.com>
2023-12-17 13:56:41 -0600

    vvvv

--- functions/test.js
+++ functions/test.js
@@ -9,8 +9,15 @@ export async function onRequest(context) {
     for (let i = 1; i < rows.length; i++) {
       const data = rows[i].split(',');
       const obj = {};
+      const entry = {
+        name: data[0].trim(),
+        age: parseInt(data[1].trim()),
+        salary: parseInt(data[2].trim()),
+        skills: data.slice(3).map((skill) => skill.trim()),
+      };
       for (let j = 0; j < headers.length; j++) {
-        obj[headers[j]] = data[j].trim();
+        // obj[headers[j]] = data[j].trim();
+        obj[headers[j]] = entry;
       }
       jsonData.push(obj);
     }
