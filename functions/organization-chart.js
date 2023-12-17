ac713e42437105afa6398abb86d6b31dfc9c713f
Chigoziri <ChigoziriOnwenu@gmail.com>
2023-12-17 13:17:33 -0600

    .

--- functions/organization-chart.js
+++ functions/organization-chart.js
@@ -1,13 +1,11 @@
 const csvFilePath = '../general_data.csv'; // Replace with your CSV file path
-const csv = require('csvtojson');
+const csv = require("csvtojson");
 
 export function onRequest(context) {
     csv()
       .fromFile(csvFilePath)
       .then((jsonObj) => {
         const jsonData = JSON.stringify(jsonObj, null, 2);
-        // Respond with JSON data
-        // res.json(jsonObj);
         return new Response(jsonObj)
       })
       .catch((err) => {
