ac5ac05cf06252e02d640b94620a11b1b558026c
Chigoziri <ChigoziriOnwenu@gmail.com>
2023-12-17 13:26:01 -0600

    try this

--- functions/organization-chart.js
+++ functions/organization-chart.js
@@ -1,16 +1,26 @@
-const csvFilePath = '../general_data.csv'; // Replace with your CSV file path
-const csv = require("csvtojson");
-
-export function onRequest(context) {
-    csv()
-      .fromFile(csvFilePath)
-      .then((jsonObj) => {
-        const jsonData = JSON.stringify(jsonObj, null, 2);
-        return new Response(jsonObj)
-      })
-      .catch((err) => {
-        console.error(err);
-        return new Response('Internal Server Error')
-      });
-   
-}
+addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event.request));
+  });
+  
+  async function handleRequest(request) {
+    const csvUrl = 'https://hiringassignment-545.pages.dev/general_data.csv'; // Replace with your CSV file URL
+    const csvContent = await fetch(csvUrl).then((res) => res.text());
+  
+    const rows = csvContent.trim().split('\n');
+    const headers = rows[0].split(',').map((header) => header.trim());
+    
+    const jsonData = [];
+    for (let i = 1; i < rows.length; i++) {
+      const data = rows[i].split(',');
+      const obj = {};
+      for (let j = 0; j < headers.length; j++) {
+        obj[headers[j]] = data[j].trim();
+      }
+      jsonData.push(obj);
+    }
+  
+    return new Response(JSON.stringify(jsonData, null, 2), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+  
